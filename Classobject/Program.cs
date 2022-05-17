63148f607e7774164ff2e00afe09d416b44f47f8
leelamani8019 <leelamani081@gmail.com>
2022-05-17 17:08:31 +0530

    commit

---
+++ Classobject/Program.cs
@@ -0,0 +1,11 @@
+﻿class animal
+{
+    public static void Main(String []args)
+    {
+        Classobject.dog dogb=new Classobject.dog();
+        dogb.breed = "bull dog";
+        Console.WriteLine(dogb.breed);
+        dogb.bark();
+        Console.ReadLine();
+    }
+}
