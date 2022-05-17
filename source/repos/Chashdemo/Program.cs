e3aa865777304c1822845f9fef144f4942b3c3b7
leelamani8019 <leelamani081@gmail.com>
2022-05-17 17:16:37 +0530

    t commit

---
+++ source/repos/Chashdemo/Program.cs
@@ -0,0 +1,10 @@
+﻿class demo
+{
+    public static void Main()
+    {
+        Chashdemo.mandiv m = new Chashdemo.mandiv();
+        m.bet(9, 8);
+        Console.ReadLine();
+       
+    }
+}
---
+++ source/repos/Chashdemo/mandiv.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chashdemo
+{
+    public class mandiv
+    {
+      public  void  bet(int a,int b)
+        {
+            int c;
+            c= a - b;
+            Console.WriteLine(c);
+        }
+    }
+}
