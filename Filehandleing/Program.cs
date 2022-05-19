a4ab04ea7bce8de7034eab430f75a00d79d6fa13
leelamani8019 <leelamani081@gmail.com>
2022-05-19 17:25:33 +0530

    commit

---
+++ Filehandleing/Program.cs
@@ -0,0 +1,13 @@
+﻿public class filedemo
+{
+    public static void Main()
+    {
+        Filehandleing.usermodule used = new Filehandleing.usermodule();
+        used.User_Module();
+        used.Category_Module();
+        used.Product_Module();
+        used.Purchase_and_Sales_Module();
+
+    }
+   
+}
---
+++ Filehandleing/usermodule.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Filehandleing
+{ 
+    public  class usermodule
+    { 
+        public void User_Module()
+            {
+                FileStream fst = new FileStream(@"E:\laxman\Filehandleing\userdetials.txt", FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fst);
+                Console.Write("Enter the UserId: ");
+                sw.Write(Console.ReadLine());
+                sw.Write(',');
+
+                Console.Write("Enter the Fname: ");
+                sw.Write(Console.ReadLine());
+                sw.Write(',');
+
+                Console.Write("Enter the Lname: ");
+                sw.Write(Console.ReadLine());
+                sw.Write(',');
+
+                Console.Write("Enter the Email: ");
+                sw.Write(Console.ReadLine());
+                sw.Write(',');
+
+                Console.Write("Enter the Phone: ");
+                sw.Write(Console.ReadLine());
+                sw.WriteLine("");
+                sw.Close();
+                fst.Close();
+            }
+            public void Category_Module()
+            {
+                FileStream fst = new FileStream(@"E:\laxman\Filehandleing\Category.txt", FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fst);
+                Console.Write("Enter the CategoryId: ");
+                sw.Write(Console.ReadLine());
+                sw.Write(',');
+
+                Console.Write("Enter the CategoryType: ");
+                sw.Write(Console.ReadLine());
+
+                sw.WriteLine("");
+                sw.Close();
+                fst.Close();
+            }
+            public void Product_Module()
+            {
+
+                FileStream fst = new FileStream(@"E:\laxman\Filehandleing\Product.txt", FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fst);
+                Console.Write("Enter the Productid: ");
+                sw.Write(Console.ReadLine());
+                sw.Write(',');
+
+                Console.Write("Enter the categorytype: ");
+                sw.Write(Console.ReadLine());
+                sw.Write(',');
+
+                Console.Write("Enter the productname: ");
+                sw.Write(Console.ReadLine());
+                sw.Write(',');
+
+                Console.Write("Enter the quantity: ");
+                sw.Write(Console.ReadLine());
+                sw.Write(',');
+
+                Console.Write("Enter the price: ");
+                sw.Write(Console.ReadLine());
+                sw.WriteLine("");
+                sw.Close();
+                fst.Close();
+
+            }
+            public void Purchase_and_Sales_Module()
+            {
+
+
+                FileStream fst = new FileStream(@"D:\DotnetTraining\DemoRepo\PurchaseandSales.txt", FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fst);
+                Console.Write("Enter the SalesId: ");
+                sw.Write(Console.ReadLine());
+                sw.Write(',');
+
+                Console.Write("Enter the Productid: ");
+                sw.Write(Console.ReadLine());
+                sw.Write(',');
+
+                Console.Write("Enter the price: ");
+                sw.Write(Console.ReadLine());
+                sw.Write(',');
+
+                Console.Write("Enter the salesdate: ");
+                sw.Write(Console.ReadLine());
+
+
+
+                sw.WriteLine("");
+                sw.Close();
+                fst.Close();
+
+
+            }
+           
+
+
+
+        }
+}
+
