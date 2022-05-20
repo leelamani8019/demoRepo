c3561aa03b6d87643886849c51be135c3a1dfad6
leelamani8019 <leelamani081@gmail.com>
2022-05-20 16:56:22 +0530

    commit

---
+++ filereding/Detials.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace filereding
+{
+    public class Detials
+    {
+        public void readfile()
+        {
+            FileStream fileStreamObj = new FileStream(@"E:\laxman\networklog.txt", FileMode.Open, FileAccess.Read);
+            StreamReader streamReaderObj = new StreamReader(fileStreamObj);
+            Console.WriteLine("Id\tsource \t destination \t date \t  time \t\t network \tstatus");
+            streamReaderObj.ReadLine();
+            while (streamReaderObj.Peek() > 0)
+            {
+                string line = streamReaderObj.ReadLine();
+                if (line != "")
+                {
+                    Console.WriteLine(line);
+
+
+                }
+
+                
+
+            }
+            Console.WriteLine("\n" + "Read operation completed");
+        }
+    }
+}
+             
---
+++ filereding/Program.cs
@@ -0,0 +1,8 @@
+﻿public class tleft
+{
+    public static void Main()
+    {
+        filereding.Detials d=new filereding.Detials();
+        d.readfile();
+    }
+}
