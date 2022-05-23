776526bf0c911a5d858ae9e1c30496ca371df0d9
leelamani8019 <leelamani081@gmail.com>
2022-05-23 11:26:48 +0530

    commit

--- filereding/Program.cs
+++ filereding/Program.cs
@@ -2,7 +2,16 @@
 {
     public static void Main()
     {
-        filereding.Detials d=new filereding.Detials();
-        d.readfile();
+        filereding.Detials det = new filereding.Detials();
+        det.readfile();
+        filereding.Retrive ret=new filereding.Retrive();
+        Console.WriteLine("Id\tsource \t destination \t date \t  time \t\t network \tstatus");
+        ret.Success();
+       // ret.missed();
+       // ret.failed();
+       // ret.dialled();
+       
+
+       
     }
 }
--- filereding/Retrive.cs
+++ filereding/Retrive.cs
@@ -8,9 +8,50 @@ namespace filereding
 {
     public class Retrive
     {
-        public void Select()
+        public void Success()
         {
+            var mydata = File.ReadAllLines(@"E:\laxman\networklog.txt");
+            foreach (String s in mydata)
+            {
+                if(s.Contains("Success"))
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+        public void missed()
+        {
+            var mydata = File.ReadAllLines(@"E:\laxman\networklog.txt");
+            foreach (String s in mydata)
+            {
+                if (s.Contains("missed"))
+                {
+                    Console.WriteLine(s);
+                }
+            }
 
+        }
+        public void failed()
+        {
+            var mydata = File.ReadAllLines(@"E:\laxman\networklog.txt");
+            foreach (String s in mydata)
+            {
+                if (s.Contains("failed"))
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+        public void dialled()
+        {
+            var mydata = File.ReadAllLines(@"E:\laxman\networklog.txt");
+            foreach (String s in mydata)
+            {
+                if (s.Contains("dailled"))
+                {
+                    Console.WriteLine(s);
+                }
+            }
         }
     }
 }
