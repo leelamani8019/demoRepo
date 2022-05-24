7b5b54cea867e0000a116bb9d55811860d8fbdb4
leelamani8019 <leelamani081@gmail.com>
2022-05-24 17:11:45 +0530

    commit

---
+++ arrayandlist/Program.cs
@@ -0,0 +1,10 @@
+﻿public class excute
+{
+    public static void Main()
+    {
+        arrayandlist.arrayclass ar= new arrayandlist.arrayclass();
+        ar.arraycl();
+        arrayandlist.createlist c=new arrayandlist.createlist();
+        c.listcl();
+    }
+}
---
+++ arrayandlist/arrayclass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayandlist
+{
+    public class arrayclass
+    {
+        public void arraycl()
+        {
+            int[] val = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            foreach (int i in val)
+            {
+                Console.WriteLine(i);
+            }
+            String[] val1 = { "tim", "martin", "nikki", "sara" };
+            foreach (string s in val1)
+            {
+                Console.WriteLine(s);
+            }
+            Boolean[] val2 = { true, false, true, false, true, false, true, false, true, false };
+            foreach (Boolean s1 in val2)
+            {
+                Console.WriteLine(s1);
+            }
+
+        }
+    }
+    public class createlist
+    {
+        public string icecream_flavours { get; set; }
+
+        public void listcl()
+        {
+            List<createlist> list = new List<createlist>();
+            createlist cr = new createlist();
+            cr.icecream_flavours = "chocolate";
+            list.Add(cr);
+            Console.WriteLine(cr.icecream_flavours);
+            cr.icecream_flavours = "vennela";
+            list.Add(cr);
+            Console.WriteLine(cr.icecream_flavours);
+            cr.icecream_flavours = "beterscoch";
+            list.Add(cr);
+            Console.WriteLine(cr.icecream_flavours);
+            cr.icecream_flavours = "pista";
+            list.Add(cr);
+            Console.WriteLine(cr.icecream_flavours);
+            cr.icecream_flavours = "blur berrey";
+            list.Add(cr);
+            Console.WriteLine(cr.icecream_flavours);
+          
+        }
+    }
+}
+   
